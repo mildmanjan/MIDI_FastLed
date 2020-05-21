16779031ba39b8805df64d4ad57d48eee25b1cf1
Spencer Marsden <mildmanjan@users.noreply.github.com>
2020-05-21 11:23:34 +0100

    Add files via upload

---
+++ MIDIUSB_buzzer_FastLED_mix.ino
@@ -0,0 +1,147 @@
+/*
+ * MIDIUSB_buzzer.ino
+ *
+ * Author: Paulo Costa
+ */ 
+
+#include <MIDIUSB.h>
+#include "pitchToFrequency.h"
+#include <FastLED.h>        //https://github.com/FastLED/FastLED
+#include <LEDMatrix.h>      //https://github.com/Jorgen-VikingGod/LEDMatrix
+
+#define BUZZ_PIN 9
+#define DATA_PIN            3
+
+#define COLOR_ORDER       GRB
+#define CHIPSET             WS2812B
+
+// initial matrix layout (to get led strip index by x/y)
+#define MATRIX_WIDTH        29
+#define MATRIX_HEIGHT       5
+#define MATRIX_TYPE         HORIZONTAL_ZIGZAG_MATRIX
+#define MATRIX_SIZE         (MATRIX_WIDTH*MATRIX_HEIGHT)
+#define NUMPIXELS           MATRIX_SIZE
+
+// create our matrix based on matrix definition
+cLEDMatrix<MATRIX_WIDTH, MATRIX_HEIGHT, MATRIX_TYPE> leds;
+uint8_t hue;
+int16_t counter;
+
+const char* pitch_name(byte pitch) {
+  static const char* names[] = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
+  return names[pitch % 12];
+}
+
+int pitch_octave(byte pitch) {
+  return (pitch / 12) - 1;
+}
+
+void noteOn(byte channel, byte pitch, byte velocity) {
+   // int16_t sx, sy, x, y;
+//  uint8_t h;
+  //tone(BUZZ_PIN, pitchFrequency[pitch]);
+     FastLED.showColor(CRGB::Red);
+    FastLED.show();
+  Serial.print("Note On: ");
+  Serial.print(pitch_name(pitch));
+  Serial.print(pitch_octave(pitch));
+  Serial.print(", channel=");
+  Serial.print(channel);
+  Serial.print(", velocity=");
+  Serial.println(velocity);
+}
+
+void noteOff(byte channel, byte pitch, byte velocity) {
+  //noTone(BUZZ_PIN);
+  //FastLED.clear();
+    FastLED.clear(true);
+  Serial.print("Note Off: ");
+  Serial.print(pitch_name(pitch));
+  Serial.print(pitch_octave(pitch));
+  Serial.print(", channel=");
+  Serial.print(channel);
+  Serial.print(", velocity=");
+  Serial.println(velocity);
+}
+
+void controlChange(byte channel, byte control, byte value) {
+  Serial.print("Control change: control=");
+  Serial.print(control);
+  Serial.print(", value=");
+  Serial.print(value);
+  Serial.print(", channel=");
+  Serial.println(channel);
+}
+
+void setup() {
+  Serial.begin(115200);  // initial LEDs
+  FastLED.addLeds<CHIPSET, DATA_PIN, COLOR_ORDER>(leds[0], leds.Size()).setCorrection(TypicalSMD5050);
+  FastLED.setCorrection(TypicalLEDStrip);
+  FastLED.setBrightness(127);
+  FastLED.clear(true);
+  delay(500);
+  FastLED.showColor(CRGB::Red);
+  delay(1000);
+  FastLED.showColor(CRGB::Lime);
+  delay(1000);
+  FastLED.showColor(CRGB::Blue);
+  delay(1000);
+  FastLED.showColor(CRGB::White);
+  delay(1000);
+  FastLED.clear(true);
+
+  hue = 0;
+  counter = 0;
+  
+}
+
+void loop() {
+    int16_t sx, sy, x, y;
+  uint8_t h;
+
+  //FastLED.clear();
+
+  h = hue;
+  
+  midiEventPacket_t rx = MidiUSB.read();
+  switch (rx.header) {
+    case 0:
+      break; //No pending events
+      
+    case 0x9:
+      noteOn(
+        rx.byte1 & 0xF,  //channel
+        rx.byte2,        //pitch
+        rx.byte3         //velocity
+      );
+        FastLED.showColor(CRGB::Red);
+      break;
+      
+    case 0x8:
+      noteOff(
+        rx.byte1 & 0xF,  //channel
+        rx.byte2,        //pitch
+        rx.byte3         //velocity
+      );
+FastLED.clear(true);
+      break;
+      
+    case 0xB:
+      controlChange(
+        rx.byte1 & 0xF,  //channel
+        rx.byte2,        //control
+        rx.byte3         //value
+      );
+      break;
+      
+    default:
+      Serial.print("Unhandled MIDI message: ");
+      Serial.print(rx.header, HEX);
+      Serial.print("-");
+      Serial.print(rx.byte1, HEX);
+      Serial.print("-");
+      Serial.print(rx.byte2, HEX);
+      Serial.print("-");
+      Serial.println(rx.byte3, HEX);
+  }
+}
